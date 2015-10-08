a0cfc76eb6c16804eeb556288c0268a62de9dd1c
Thomas Fanell <thomas.fanell@vidhance.com>
2015-10-08 16:17:29 +0200

    SyntaxTree/source now saves every token it reads

--- source/SyntaxTree/Source.ts
+++ source/SyntaxTree/Source.ts
@@ -1,14 +1,27 @@
 /// <reference path="../Error/Region" />
 /// <reference path="../Error/Handler" />
 /// <reference path="../Utilities/Iterator" />
-/// <reference path="../Utilities/BufferedIterator" />
 /// <reference path="../Tokens/Token" />
 /// <reference path="../Tokens/Substance" />
 
 module Magic.SyntaxTree {
-	export class Source extends Utilities.BufferedIterator<Tokens.Substance> implements Error.Handler {
-		constructor(backend: Utilities.Iterator<Tokens.Substance>, private errorHandler: Error.Handler) {
-			super(backend)
+	export class Source implements Utilities.Iterator<Tokens.Substance>, Error.Handler {
+		private buffer: Tokens.Substance[] = []
+		private readTokens: Tokens.Substance[] = []
+		constructor(private backend: Utilities.Iterator<Tokens.Substance>, private errorHandler: Error.Handler) {
+		}
+		peek(position: number = 0): Tokens.Substance {
+			var next: Tokens.Substance = null
+			while (position > this.buffer.length - 1 && (next = this.backend.next())) {
+				this.buffer.push(next)
+			}
+			return position > this.buffer.length - 1 ? null : this.buffer[position]
+		}
+		next(): Tokens.Substance {
+			var result = this.peek()
+			if (this.buffer.length > 0)
+				this.readTokens.push(this.buffer.shift())
+			return result
 		}
 		raise(message: string | Error.Message, level = Error.Level.Critical, type = Error.Type.Gramatical, region?: Error.Region): void {
 			if (typeof message == "string") {
