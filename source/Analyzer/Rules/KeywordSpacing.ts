4de8a48ff59e19471b3754bba5bc1ce1c4ecf2c6
Thomas Fanell <thomas.fanell@vidhance.com>
2015-11-09 10:37:40 +0100

    Allow keyword to be immediatly followed by the * operator

--- source/Analyzer/Rules/KeywordSpacing.ts
+++ source/Analyzer/Rules/KeywordSpacing.ts
@@ -52,7 +52,8 @@ module Magic.Analyzer.Rules {
 							case Frontend.TokenKind.OperatorGreaterThan: // Allow This<T> etc.
 							case Frontend.TokenKind.OperatorConditional: // Allow in? etc.
 							case Frontend.TokenKind.SeparatorColon: // Allow func(class: foo) etc.
-							case Frontend.TokenKind.OperatorMultiply: // Allow This*
+								break;
+							case Frontend.TokenKind.OperatorMultiply: // Allow 'Keyword*'
 								break;
 							default:
 								report.addViolation(new Violation(right.location,
